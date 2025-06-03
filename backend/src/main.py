8d0989bdb07dafc6210e38757ca9bb416f953485
suguru shiroishi <s.shiroishi@branu.jp>
2025-06-03 19:09:50 +0900

    fjf

--- backend/src/main.py
+++ backend/src/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://branubrain-fs-dev.web.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
