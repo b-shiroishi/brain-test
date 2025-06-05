b0a01f882c1bc715433f596a37803cf7d59345df
suguru shiroishi <s.shiroishi@branu.jp>
2025-06-05 17:51:18 +0900

    ffd

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://branubrain-fs-dev-87a12ee7.web.app/", "https://branubrain-fs-dev-87a12ee7.firebaseapp.com/"],
+    allow_origins=["http://localhost:5173", "https://branubrain-fs-dev-87a12ee7.web.app", "https://branubrain-fs-dev-87a12ee7.firebaseapp.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
